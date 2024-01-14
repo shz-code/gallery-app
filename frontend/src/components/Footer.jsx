a97ba4dac9fbb0df3d4a36cc50d1be6cc07d6e84
shz-code <web.shahidul.alam@gmail.com>
2024-01-14 22:17:33 +0600

    modify: footer (Footer.jsx)

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,11 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-slate-950 text-white py-2 mt-4">
+      <div className="container mx-auto px-2 text-center">
+        All Rights Reserved.
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
